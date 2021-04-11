929dc9c680affb047493dff05a930b419101eee1
Lourivaldo Vasconcelos <lourivaldo@mangue3.com.br>
2021-04-11 16:38:03 -0300

    Ficalizacoes filtragem

--- src/fiscalizacoes/fiscalizacoes.repository.ts
+++ src/fiscalizacoes/fiscalizacoes.repository.ts
@@ -5,26 +5,29 @@ import { Injectable } from '@nestjs/common';
 export class FiscalizacoesRepository {
   async index(page: number, type: string, search?: string): Promise<any[]> {
     const entityManager = getManager();
-
+    search = search && search.trim()
+    // console.log('page', page);
+    // console.log('type', type);
+    // console.log('search', search);
     let fileQuery;
 
     if (type == 'recent' && search) {
       const query = `
-    SELECT * 
-    FROM fiscalizacoes
-    WHERE sit_proc NOT IN ('ARQUIVADO')
-      AND (
-      denunciado ILIKE $3
-      OR nome ILIKE $3
-      OR logradouro ILIKE $3
-      OR bairro ILIKE $3
-      OR municipio ILIKE $3
-      OR bacia ILIKE $3
-      OR uso ILIKE $3
-      )
-    ORDER BY REVERSE(dt_entrada) DESC
-    OFFSET $1
-    LIMIT $2`
+      SELECT * 
+      FROM fiscalizacoes
+      WHERE sit_proc NOT IN ('ARQUIVADO')
+        AND (
+        denunciado ILIKE $3
+        OR nome ILIKE $3
+        OR logradouro ILIKE $3
+        OR bairro ILIKE $3
+        OR municipio ILIKE $3
+        OR bacia ILIKE $3
+        OR uso ILIKE $3
+        )
+      ORDER BY REVERSE(dt_entrada) DESC
+      OFFSET $1
+      LIMIT $2`
       fileQuery = entityManager.query(query,[
         100 * (page - 1),
         100,
@@ -33,35 +36,86 @@ export class FiscalizacoesRepository {
 
     } else if (type == 'recent') {
       const query = `
-    SELECT * 
-    FROM fiscalizacoes
-    WHERE sit_proc NOT IN ('ARQUIVADO')
-    ORDER BY REVERSE(dt_entrada) DESC
-    OFFSET $1
-    LIMIT $2`
+      SELECT * 
+      FROM fiscalizacoes
+      WHERE sit_proc NOT IN ('ARQUIVADO')
+      ORDER BY REVERSE(dt_entrada) DESC
+      OFFSET $1
+      LIMIT $2`
       fileQuery = entityManager.query(query,[
         100 * (page - 1),
         100
       ]);
 
-    } /*else if (type == 'archived') {
-      query = `
-    SELECT * 
-    FROM fiscalizacoes
-    ORDER BY REVERSE(dt_entrada) DESC
-    WHERE sit_proc = $3
-    OFFSET $1
-    LIMIT $2`
-    } else if (type == '') {
-      query = `
-    SELECT * 
-    FROM fiscalizacoes
-    ORDER BY RANDOM()
-    OFFSET $1
-    LIMIT $2`
-    }*/
-
-
+    } else if ((type == 'all' || type == 'nearby' || type == 'favorite') && search) {
+      const query = `
+      SELECT * 
+      FROM fiscalizacoes
+      WHERE sit_proc NOT IN ('ARQUIVADO')
+      AND (
+        denunciado ILIKE $3
+        OR nome ILIKE $3
+        OR logradouro ILIKE $3
+        OR bairro ILIKE $3
+        OR municipio ILIKE $3
+        OR bacia ILIKE $3
+        OR uso ILIKE $3
+        )
+      ORDER BY REVERSE(dt_entrada) DESC
+      OFFSET $1
+      LIMIT $2`
+      fileQuery = entityManager.query(query,[
+        100 * (page - 1),
+        100,
+        `%${search}%`,
+      ]);
+    } else if (type == 'all' || type == 'nearby' || type == 'favorite') {
+      const query = `
+      SELECT * 
+      FROM fiscalizacoes
+      WHERE sit_proc NOT IN ('ARQUIVADO')
+      ORDER BY REVERSE(dt_entrada) DESC
+      OFFSET $1
+      LIMIT $2`
+      fileQuery = entityManager.query(query,[
+        100 * (page - 1),
+        100
+      ]);
+    } else if (type == 'archived' && search) {
+      const query = `
+      SELECT * 
+      FROM fiscalizacoes
+      WHERE sit_proc IN ('ARQUIVADO')
+        AND (
+        denunciado ILIKE $3
+        OR nome ILIKE $3
+        OR logradouro ILIKE $3
+        OR bairro ILIKE $3
+        OR municipio ILIKE $3
+        OR bacia ILIKE $3
+        OR uso ILIKE $3
+        )
+      ORDER BY REVERSE(dt_entrada) DESC
+      OFFSET $1
+      LIMIT $2`
+      fileQuery = entityManager.query(query,[
+        100 * (page - 1),
+        100,
+        `%${search}%`
+      ]);
+    } else if (type == 'archived') {
+      const query = `
+      SELECT * 
+      FROM fiscalizacoes
+      WHERE sit_proc IN ('ARQUIVADO')
+      ORDER BY REVERSE(dt_entrada) DESC
+      OFFSET $1
+      LIMIT $2`
+      fileQuery = entityManager.query(query,[
+        100 * (page - 1),
+        100
+      ]);
+    }
 
     const result = await fileQuery;
 

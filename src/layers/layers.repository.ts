3b997117cac88ba3293aefd4d6e843cbba28e607
Lourivaldo Vasconcelos <lourivaldo@mangue3.com.br>
2021-05-02 15:46:45 -0300

    TCC

--- src/layers/layers.repository.ts
+++ src/layers/layers.repository.ts
@@ -18,13 +18,14 @@ SELECT json_build_object(
     'features', json_agg(
         json_build_object(
             'type',       'Feature',
-            'id',         'id', -- the GeoJson spec includes an 'id' field, but it is optional, replace {id} with your id field
+            'id',         "id",
             'geometry',   ST_AsGeoJSON(geom)::json,
             'properties', json_build_object(
-                'geom', 
-                'to',
-                'nome', 
-                'cep'
+                'type', 'fiscalizacao',
+                'title', 'Fiscalizações',
+                'pf', "proc.fisc.",
+                'dt', "dt_entrada",
+                'pa', "proc_outor"
             )
         )
     )
@@ -55,13 +56,12 @@ SELECT json_build_object(
     'features', json_agg(
         json_build_object(
             'type',       'Feature',
-            'id',         'id', -- the GeoJson spec includes an 'id' field, but it is optional, replace {id} with your id field
+            'id',         "id",
             'geometry',   ST_AsGeoJSON(geom)::json,
             'properties', json_build_object(
-                'geom', 
-                'to',
-                'nome', 
-                'cep'
+                'type', 'municipio',
+                'title', 'Municipios',
+                'name', "nm_municip"
             )
         )
     )
@@ -92,18 +92,234 @@ SELECT json_build_object(
     'features', json_agg(
         json_build_object(
             'type',       'Feature',
-            'id',         'id', -- the GeoJson spec includes an 'id' field, but it is optional, replace {id} with your id field
+            'id',         "id",
             'geometry',   ST_AsGeoJSON(geom)::json,
-            'properties', json_build_object(
-                'geom', 
-                'to',
-                'nome', 
-                'cep'
+            'properties', json_build_object(            
+                'type', 'hidrografia',
+                'title', 'Hidrografia',
+                'name', "name", 
+                'rio', "rioscsv_ba"
             )
         )
     )
 )
 FROM hidrografia`,
+      []);
+
+    const result = await fileQuery;
+
+    if (result && result[0] && result[0]['json_build_object']) {
+      return result[0]['json_build_object'];
+    }
+
+    return null;
+  }
+
+  async getMunicipiosEmergencia(): Promise<string> {
+    const entityManager = getManager();
+    const fileQuery = entityManager.query(`
+SELECT json_build_object(
+    'type', 'FeatureCollection',
+    'crs',  json_build_object(
+        'type',      'name', 
+        'properties', json_build_object(
+            'name', 'EPSG:4326'  
+        )
+    ), 
+    'features', json_agg(
+        json_build_object(
+            'type',       'Feature',
+            'id',         "id",
+            'geometry',   ST_AsGeoJSON(geom)::json,
+            'properties', json_build_object(            
+                'type', 'municipios_emergencia',
+                'title', 'Municipios Emergencia',
+                'name', "NM_MUNICIP"
+            )
+        )
+    )
+)
+FROM municipios_emergencia`,
+      []);
+
+    const result = await fileQuery;
+
+    if (result && result[0] && result[0]['json_build_object']) {
+      return result[0]['json_build_object'];
+    }
+
+    return null;
+  }
+
+  async getFederalRios(): Promise<string> {
+    const entityManager = getManager();
+    const fileQuery = entityManager.query(`
+SELECT json_build_object(
+    'type', 'FeatureCollection',
+    'crs',  json_build_object(
+        'type',      'name', 
+        'properties', json_build_object(
+            'name', 'EPSG:4326'  
+        )
+    ), 
+    'features', json_agg(
+        json_build_object(
+            'type',       'Feature',
+            'id',         "id",
+            'geometry',   ST_AsGeoJSON(geom)::json,
+            'properties', json_build_object(            
+                'type', 'federal_rios',
+                'title', 'Federal Rios',
+                'name', "NORIOORIGI"
+            )
+        )
+    )
+)
+FROM federal_rios`,
+      []);
+
+    const result = await fileQuery;
+
+    if (result && result[0] && result[0]['json_build_object']) {
+      return result[0]['json_build_object'];
+    }
+
+    return null;
+  }
+
+  async getFederalEspelhosDagua(): Promise<string> {
+    const entityManager = getManager();
+    const fileQuery = entityManager.query(`
+SELECT json_build_object(
+    'type', 'FeatureCollection',
+    'crs',  json_build_object(
+        'type',      'name', 
+        'properties', json_build_object(
+            'name', 'EPSG:4326'  
+        )
+    ), 
+    'features', json_agg(
+        json_build_object(
+            'type',       'Feature',
+            'id',         "id",
+            'geometry',   ST_AsGeoJSON(geom)::json,
+            'properties', json_build_object(            
+                'type', 'federal_espelhos_dagua',
+                'title', 'Federal Espelhos D Agua',
+                'name', "NOME_RESER"
+            )
+        )
+    )
+)
+FROM federal_espelhos_dagua`,
+      []);
+
+    const result = await fileQuery;
+
+    if (result && result[0] && result[0]['json_build_object']) {
+      return result[0]['json_build_object'];
+    }
+
+    return null;
+  }
+
+  async getBacias(): Promise<string> {
+    const entityManager = getManager();
+    const fileQuery = entityManager.query(`
+SELECT json_build_object(
+    'type', 'FeatureCollection',
+    'crs',  json_build_object(
+        'type',      'name', 
+        'properties', json_build_object(
+            'name', 'EPSG:4326'  
+        )
+    ), 
+    'features', json_agg(
+        json_build_object(
+            'type',       'Feature',
+            'id',         "id",
+            'geometry',   ST_AsGeoJSON(geom)::json,
+            'properties', json_build_object(            
+                'type', 'bacias',
+                'title', 'Bacias',
+                'name', "Name"
+            )
+        )
+    )
+)
+FROM bacias`,
+      []);
+
+    const result = await fileQuery;
+
+    if (result && result[0] && result[0]['json_build_object']) {
+      return result[0]['json_build_object'];
+    }
+
+    return null;
+  }
+
+  async getFederalTerrasIndigenasPe(): Promise<string> {
+    const entityManager = getManager();
+    const fileQuery = entityManager.query(`
+SELECT json_build_object(
+    'type', 'FeatureCollection',
+    'crs',  json_build_object(
+        'type',      'name', 
+        'properties', json_build_object(
+            'name', 'EPSG:4326'  
+        )
+    ), 
+    'features', json_agg(
+        json_build_object(
+            'type',       'Feature',
+            'id',         "id",
+            'geometry',   ST_AsGeoJSON(geom)::json,
+            'properties', json_build_object(            
+                'type', 'federal_terras_indigenas_pe',
+                'title', 'Federal Terras Indigenas',
+                'name', "terrai_nom"
+            )
+        )
+    )
+)
+FROM federal_terras_indigenas_pe`,
+      []);
+
+    const result = await fileQuery;
+
+    if (result && result[0] && result[0]['json_build_object']) {
+      return result[0]['json_build_object'];
+    }
+
+    return null;
+  }
+
+  async getUcProtecaoIntegral(): Promise<string> {
+    const entityManager = getManager();
+    const fileQuery = entityManager.query(`
+SELECT json_build_object(
+    'type', 'FeatureCollection',
+    'crs',  json_build_object(
+        'type',      'name', 
+        'properties', json_build_object(
+            'name', 'EPSG:4326'  
+        )
+    ), 
+    'features', json_agg(
+        json_build_object(
+            'type',       'Feature',
+            'id',         "id",
+            'geometry',   ST_AsGeoJSON(geom)::json,
+            'properties', json_build_object(            
+                'type', 'uc_protecao_integral',
+                'title', 'UC Protecao Integral',
+                'name', "Name"
+            )
+        )
+    )
+)
+FROM uc_protecao_integral`,
       []);
 
     const result = await fileQuery;
--- src/layers/layers.service.ts
+++ src/layers/layers.service.ts
@@ -8,10 +8,13 @@ export class LayersService {
   private config = {
     '5ff3d198-5c1c-4107-b57d-8a9d6e26a8fe': 'getFiscalizacoes',
     'ddc3b31f-bae5-43b4-9795-08a6e3f0079e': 'getMunicipios',
-    'd1e2108f-9e05-4803-bb6c-540dab4f72c4': 'federal_rios',
     '43c574c3-d85d-43c0-a607-2a443e6ae63c': 'getHidrografia',
-    // 'b32426d2-ad1a-40ad-9955-ca91ba97bcb8': '',
-    // '9ae50c01-5fe1-41ea-bd10-6876d3abfd44': '',
+    'b32426d2-ad1a-40ad-9955-ca91ba97bcb8': 'getMunicipiosEmergencia',
+    '9ae50c01-5fe1-41ea-bd10-6876d3abfd44': 'getFederalRios',
+    '9ae50c01-5fe1-41ea-bd10-6876d3abfd48': 'getFederalEspelhosDagua',
+    '9ae50c01-5fe1-41ea-bd10-6876d3abfd45': 'getBacias',
+    '9ae50c01-5fe1-41ea-bd10-6876d3abfd46': 'getFederalTerrasIndigenasPe',
+    '9ae50c01-5fe1-41ea-bd10-6876d3abfd47': 'getUcProtecaoIntegral',
   }
 
   constructor(private repository: LayersRepository) {}
